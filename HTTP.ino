89d3551b857b3af6467f165defa405756920b041
Max <max30012540@hotmail.com>
2019-08-29 21:57:00 +0700

    แก้ไขให้เพิ่ม Header ได้

--- HTTP.ino
+++ HTTP.ino
@@ -8,6 +8,12 @@ bool HTTPRequest(uint8_t data) {
   static uint8_t method = 0; // 0: GET, 1: PATCH, 2: POST, 3: PUT
   static uint8_t payload_len = 0;
   static String payload = "";
+  static uint8_t headerCount = 0;
+  static uint8_t headerNameLen = 0;
+  static uint8_t headerValueLen = 0;
+  static uint8_t headerInx = 0;
+  static String headerName[5];
+  static String headerValue[5];
 
   if (state == 0) {
     url_len = data;
@@ -22,6 +28,7 @@ bool HTTPRequest(uint8_t data) {
   } else if (state == 2) {
     method = data;
     state = 3;
+  // Payload
   } else if (state == 3) {
     payload_len = data;
     payload = "";
@@ -34,13 +41,59 @@ bool HTTPRequest(uint8_t data) {
     if (payload_len <= 0) {
       state = 5;
     }
+  // Add Header
   } else if (state == 5) {
+    headerCount = data;
+    if (headerCount == 0) {
+      state = 20; // go to do state
+    } else {
+      headerInx = 0;
+      state = 6;
+    }
+  } else if (state == 6) {
+    headerNameLen = data;
+    headerName[headerInx] = "";
+    state = 7;
+  } else if (state == 7) {
+    headerName[headerInx] += (char)data;
+    headerNameLen--;
+    if (headerNameLen <= 0) {
+      state = 8;
+    }
+  } else if (state == 8) {
+    headerValueLen = data;
+    headerValue[headerInx] = "";
+    state = 7;
+  } else if (state == 9) {
+    headerValue[headerInx] += (char)data;
+    headerValueLen--;
+    if (headerValueLen <= 0) {
+      headerInx++;
+      if (headerInx >= headerCount) {
+        state = 20;
+      } else {
+        state = 6;
+      }
+    }
+  } else if (state == 20) { // dumy data
     Serial1.write(0x1F);
     Serial1.write(0xF1);
     Serial1.write(0x20); // HTTP Request response
 
+    WiFiClientSecure *client;
     HTTPClient http;
-    http.begin(url);
+    
+    if (url.startsWith("https")) {
+      client = new WiFiClientSecure;
+      http.begin(*client, url);
+    } else {
+      http.begin(url);
+    }
+
+    for (int i=0;i<headerCount;i++) {
+      http.addHeader(headerName[i], headerValue[i]);
+    }
+    
     uint16_t httpCode;
     if (method == 0) {
       httpCode = http.GET();
@@ -58,7 +111,7 @@ bool HTTPRequest(uint8_t data) {
     Serial1.write((uint8_t)rosPayload_len >> 8);
     Serial1.write((uint8_t)rosPayload_len & 0xFF);
     Serial1.print(rosPayload);
-
+    http.end();
     state = 0;
     res = true;
   }
